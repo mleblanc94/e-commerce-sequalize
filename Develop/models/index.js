431c8e10bf2d6c4905b672b09bae19832706a024
Michael Leblanc <mleblanc1@werfen.com>
2023-10-19 19:36:57 -0400

    added to index file for models and added relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, { 
+  foreignKey: 'category_id',
+  targetKey: 'id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  targetKey: 'id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+  } 
+);
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+})
 
 module.exports = {
   Product,
