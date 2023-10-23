b55ee171a4a66c0c46b0be771bbdcb818cb63422
Michael Leblanc <mleblanc1@werfen.com>
2023-10-23 13:53:52 -0400

    added routes for categories

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -29,9 +29,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
   });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{
+        model: Category,
+        attributes: ["id", "category_name"],
+        },
+        {
+        model: Tag,
+        as: 'tags',
+        through: {
+          model: ProductTag,
+          attributes: ['id', 'product_id', 'tag_id'],
+        }
+        },
+      ]
+    })
+    res.json(productData);
+} catch (error) {
+  console.error(error);
+}
 });
 
 // create new product
@@ -111,8 +131,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
